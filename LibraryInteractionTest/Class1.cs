7c045d631be76e0b0ed61972ad6fc84ca9331da2
Riley Converse <riley.converse@yahoo.com>
2024-01-09 14:35:34 -0500

    Initial commit

---
+++ LibraryInteractionTest/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace LibraryInteractionTest
+{
+    public class Class1
+    {
+        private readonly string[] _contents;
+        private int _index;
+        public Class1(params string[] args)
+        {
+            _contents = args;
+            _index = 0;
+        }
+
+        public string? OutputText()
+        {
+            if (_index < _contents.Length)
+            {
+                _index++;
+                return _contents[_index - 1];
+            }
+            else
+            {
+                _index = 0;
+                return null;
+            }
+        }
+
+    }
+}
